553e9788006713daf6ae284b317a21afb971366a
Tony Wang <zw3144@columbia.edu>
2025-12-18 15:28:01 -0500

    add short shell to start gui (on desktop)

---
+++ Isaac_Lab_Files/start_gui.sh
@@ -0,0 +1,4 @@
+../../IsaacLab/isaaclab.sh -p ../../IsaacLab/scripts/tutorials/00_sim/create_empty.py
+
+# python scripts/reinforcement_learning/sb3/train.py --task=Foosball-1player-v0 --headless  --video  --video_length 500  --video_interval 10000
+# python scripts/reinforcement_learning/sb3/train.py --task=Foosball-1player-v0  --video  --video_length 500  --video_interval 10000
